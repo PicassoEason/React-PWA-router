0ef855e642cc2d4d42a55462634bb68f890327fa
picassoeason <easonwu.student@outlook.com>
2024-04-30 20:57:42 +0800

    footer 2

--- src/component/Footer/Footer_1.js
+++ src/component/Footer/Footer_1.js
@@ -4,20 +4,28 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 export default function Footer() {
   return (
     <>
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-              <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 Company, Inc</p>
+        <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+              <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Company, Inc</p>
 
-              <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+              <Link to="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                {/* 在這裡放置您想要包含的內容 */}
+              </Link>
 
-              </a>
-
-              <ul class="nav col-md-4 justify-content-end">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+              <ul className="nav col-md-4 justify-content-end">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link px-2 text-body-secondary">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link px-2 text-body-secondary">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/content" className="nav-link px-2 text-body-secondary">content</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/*" className="nav-link px-2 text-body-secondary">NoPage</Link>
+                </li>
+                {/* <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li> */}
               </ul>
             </footer>
         </div>
--- src/component/Footer/Footer_2.js
+++ src/component/Footer/Footer_2.js
@@ -3,16 +3,23 @@ import { Link } from 'react-router-dom'; // 導入 Link 元件
 export default function Footer() {
     return (
       <>
-        <div class="container">
-            <footer class="py-3 my-4">
-              <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+        <div className="container">
+            <footer className="py-3 my-4">
+              <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+              <li className="nav-item">
+                  <Link to="/" className="nav-link px-2 text-body-secondary">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link px-2 text-body-secondary">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/content" className="nav-link px-2 text-body-secondary">content</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/*" className="nav-link px-2 text-body-secondary">NoPage</Link>
+                </li>
               </ul>
-              <p class="text-center text-body-secondary">&copy; 2024 Company, Inc</p>
+              <p className="text-center text-body-secondary">&copy; 2024 Company, Inc</p>
             </footer>
         </div>
       </>
